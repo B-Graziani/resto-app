9f486ea44bdc30533a28cbd05c9fa6d429e3fde1
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-05-03 14:13:08 +0200

    donedone

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,22 @@ class ReviewsController < ApplicationController
     @article = Article.find(params[:article_id])
     @review = Review.new
   end
+  def create
+    @article = Article.find(params[:article_id])
+    @review = Review.new(review_params)
+    @review.article = @article
+    @review.save
+    redirect_to @article
+  end
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to @review.article
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
 end
